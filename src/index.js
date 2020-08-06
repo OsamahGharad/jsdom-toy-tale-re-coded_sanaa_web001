5d680eec7c4ea21a101752b1c26ec1e871866fb0
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-06 10:14:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,4 +12,29 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const toy-collection =document.querySelector('#toy-collection'#);
+
+  function fetchToys(){
+    const toy_image_url=(http:localhost:3000/toys);
+      fetch(toy_image_url).then(response =>response.json()).then(result =>displayData(result));
+      
+  }
+  function displayData(data){
+    const div =document.creatElement('div');
+    div.addClass('card');
+    for( element of data){
+      const h2 = document.creatElement('h2');
+      h2.innerHTML=data['name'];
+      const img= document.creatElement('img');
+      img.src='name';
+      const p =document.creatElement('p');
+      p.innerHTML="likes";
+      const button =document.creatElement('button');
+      button.addClass('like-btn');
+    }
+  
+    
+  }
 });
+
