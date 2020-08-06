6d21e2bfe39f9374db32a92ac6d581ca81dd74e2
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-06 10:14:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function fetchToys(){
     const toy_image_url=(http:localhost:3000/toys);
       fetch(toy_image_url).then(response =>response.json()).then(result =>displayData(result));
-      
+
   }
   function displayData(data){
     const div =document.creatElement('div');
@@ -33,8 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const button =document.creatElement('button');
       button.addClass('like-btn');
     }
-  
-    
+
+
   }
 });
-
